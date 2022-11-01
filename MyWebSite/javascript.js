b92032c8948d4d957f9813b032bf3890275f50c7
DayKnight <48128587+Kouly-productions@users.noreply.github.com>
2022-11-01 10:19:16 +0100

    Added (Om Mig) Button, and loading effect

--- MyWebSite/javascript.js
+++ MyWebSite/javascript.js
@@ -1,26 +1,3 @@
-function show_hide() {
-    var x = document.getElementById("what");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function show_hidee() {
-    var x = document.getElementById("whatt");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function show_hideee() {
-    var x = document.getElementById("whattt");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+$(window).on("load",function(){
+  $(".loader-wrapper").fadeOut("slow");
+});
